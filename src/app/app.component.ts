7ba28d505acce81d12c4ab3651fcdb34c3e871e2
ethwillz <ewwilliams6@gmail.com>
2017-10-07 17:41:54 -0500

    Goit input component so that I can intercept user commnands on enter keypress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+
+  @ViewChild('command') commandElement;
+
+  handleKeypress (event: any) {
+    let command = this.commandElement.nativeElement.value;
+    if(event.keyCode == 13){
+      alert(command);
+    }
+  }
+
 }
