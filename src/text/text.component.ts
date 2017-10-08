70f39445bf57ac64b14a5baee70861fc965a91cd
ethwillz <ewwilliams6@gmail.com>
2017-10-07 20:22:41 -0500

    Forgot to add files

---
+++ src/text/text.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'text-component',
+  templateUrl: './text.component.html',
+  styleUrls: ['./text.component.css']
+})
+
+export class TextComponent {
+
+}
